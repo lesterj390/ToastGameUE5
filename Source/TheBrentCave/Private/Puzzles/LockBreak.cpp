8d2962f4a2fa9ebc3f9e4356d095c19c6a37ddef
ethaanw <ethanwaddell8@gmail.com>
2023-01-10 21:28:56 -0500

    Rewrote Lock Puzzle in C++
    Helped optimize puzzle and clean up code
    Helped fix issues where you couldn't exit lock puzzle after entering

---
+++ Source/TheBrentCave/Private/Puzzles/LockBreak.cpp
@@ -0,0 +1,302 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Puzzles/LockBreak.h"
+
+// Sets default values
+ALockBreak::ALockBreak()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+
+	Pivot = CreateDefaultSubobject<USceneComponent>(TEXT("Pivot"));
+	Pivot->SetupAttachment(RootComponent);
+
+	ChestTopMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Chest Top Mesh"));
+	ChestTopMesh->SetupAttachment(Pivot);
+
+	ChestBottomMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Chest Bottom Mesh"));
+	ChestBottomMesh->SetupAttachment(Pivot);
+
+	LockBaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Lock Base Mesh"));
+	LockBaseMesh->SetupAttachment(Pivot);
+
+	LockDialMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Lock Dial Mesh"));
+	LockDialMesh->SetupAttachment(Pivot);
+
+	LockHookMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Lock Hook Mesh"));
+	LockHookMesh->SetupAttachment(Pivot);
+
+	Hitbox = CreateDefaultSubobject<UBoxComponent>(TEXT("Hitbox"));
+	Hitbox->SetupAttachment(Pivot);
+
+	PuzzleCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	PuzzleCamera->SetupAttachment(Pivot);
+
+	Spotlight = CreateDefaultSubobject<USpotLightComponent>(TEXT("Spotlight"));
+	Spotlight->SetupAttachment(Pivot);
+
+	InteractComponent = CreateDefaultSubobject<UWidgetComponent>(TEXT("Interact Widget"));
+	InteractComponent->SetupAttachment(Pivot);
+
+	LockLevel = 1;
+
+	ComboRecentlyPlayed = false;
+
+	clockwise = true;
+
+	inOverlap = false;
+	inPuzzle = false;
+}
+
+// Called when the game starts or when spawned
+void ALockBreak::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	Spotlight->SetVisibility(false);
+	InteractComponent->SetVisibility(false);
+	ResetDialTimeline.SetLooping(false);
+
+	SetupPuzzle();
+}
+
+void ALockBreak::SetupTimelines()
+{
+	// Puzzle Timeline
+	// Setup Curve
+	if (DialCurve) {
+		FOnTimelineFloat TimelineProgress;
+		TimelineProgress.BindUFunction(this, FName("UpdateTimelineProgress"));
+		DialTimeline.AddInterpFloat(DialCurve, TimelineProgress);
+	}
+
+	// Reset Puzzle Timeline
+	// Setup Curve
+	if (ResetDialCurve) {
+		FOnTimelineFloat ResetTimelineProgress;
+		ResetTimelineProgress.BindUFunction(this, FName("UpdateResetTimelineProgress"));
+		ResetDialTimeline.AddInterpFloat(ResetDialCurve, ResetTimelineProgress);
+	}
+
+	// Setup On Complete Event
+	FOnTimelineEvent ResetTimelineComplete;
+	ResetTimelineComplete.BindUFunction(this, FName("ResetLock"));
+	ResetDialTimeline.SetTimelineFinishedFunc(ResetTimelineComplete);
+}
+
+void ALockBreak::UpdateTimelineProgress(float Value)
+{
+	float angle = FMath::Lerp(0, 359, Value);
+	DialLocation = angle;
+	LockDialMesh->SetRelativeRotation((FRotator(angle, 90.0f, 0.0f)));
+
+	if (LockLevel <= LockLength) {
+		if (DialLocation >= LockCode[LockLevel - 1] && DialLocation <= LockCode[LockLevel - 1] + PickRange && !ComboRecentlyPlayed) {
+			ComboRecentlyPlayed = true;
+
+			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Cyan, "On Combo");
+
+			UAudioComponent* SoundComponent = UGameplayStatics::CreateSound2D(GetWorld(), OnComboSound);
+			SoundComponent->Play(0.0);
+		}
+		else {
+			ComboRecentlyPlayed = false;
+		}
+	}
+}
+
+void ALockBreak::UpdateResetTimelineProgress(float Value)
+{
+	float angle = FMath::Lerp(0, 1080, Value);
+	LockDialMesh->SetRelativeRotation((FRotator(angle, 90.0f, 0.0f)));
+}
+
+void ALockBreak::SetupPuzzle()
+{
+	SetupTimelines();
+	SetupPlayerInput();
+	GenerateLockCode();
+}
+
+void ALockBreak::SetupPlayerInput()
+{
+	Hitbox->OnComponentBeginOverlap.AddDynamic(this, &ALockBreak::OnOverlapStart);
+	Hitbox->OnComponentEndOverlap.AddDynamic(this, &ALockBreak::OnOverlapEnd);
+
+	InputComponent = NewObject<UInputComponent>(this);
+	InputComponent->RegisterComponent();
+
+	InputComponent->BindAction("Interact", IE_Pressed, this, &ALockBreak::Interact);
+	InputComponent->BindAction("Exit", IE_Pressed, this, &ALockBreak::ExitPuzzle);
+	DisableInput(GetWorld()->GetFirstPlayerController());
+}
+
+void ALockBreak::Interact()
+{
+	if (inPuzzle) {
+		LockPickCheck();
+	}
+	else {
+		EnterPuzzle();
+	}
+}
+
+void ALockBreak::EnterPuzzle()
+{
+	inPuzzle = true;
+
+	DialTimeline.Stop();
+	ResetDialTimeline.Stop();
+
+	ResetDialTimeline.PlayFromStart();
+
+	if (HitBoxPlayer)
+	{
+		GetWorld()->GetFirstPlayerController()->SetViewTargetWithBlend(this, 0.5f);
+		InteractComponent->SetVisibility(false);
+		Spotlight->SetVisibility(true);
+
+		HitBoxPlayer->SetActorHiddenInGame(true);
+		HitBoxPlayer->SetActorEnableCollision(false);
+		EnableInput(GetWorld()->GetFirstPlayerController());
+	}
+}
+
+void ALockBreak::ExitPuzzle()
+{
+	if (HitBoxPlayer)
+	{
+		inPuzzle = false;
+
+		Spotlight->SetVisibility(false);
+		HitBoxPlayer->SetActorHiddenInGame(false);
+		HitBoxPlayer->SetActorEnableCollision(true);
+		InteractComponent->SetVisibility(true);
+
+		GetWorld()->GetFirstPlayerController()->SetViewTargetWithBlend(HitBoxPlayer, 0.5f);
+
+		ResetLock();
+	}
+}
+
+void ALockBreak::ResetLock()
+{
+	ResetDialTimeline.Stop();
+	DialTimeline.Stop();
+
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Cyan, "Function ResetLock was called.");
+
+	LockLevel = 1;
+	clockwise = true;
+
+	ComboRecentlyPlayed = false;
+
+	if (inPuzzle) {
+		GetWorldTimerManager().SetTimer(ResetCooldown, this, &ALockBreak::StartPuzzle, 0.5, false);
+	}
+	else {
+		LockDialMesh->SetRelativeRotation((FRotator(0.0f, 90.0f, 0.0f)));
+	}
+}
+
+void ALockBreak::StartPuzzle()
+{
+	DialTimeline.PlayFromStart();
+	DialTimeline.SetLooping(true);
+}
+
+void ALockBreak::GenerateLockCode()
+{
+	LockCode.Empty();
+
+	int MaxLockPick = 359 - PickRange;
+
+	for (int i = 0; i < LockLength; i++) {
+		LockCode.Add(FMath::RandRange(0, MaxLockPick));
+	}
+}
+
+void ALockBreak::LockPickCheck()
+{
+	if (LockLevel <= LockLength) {
+		if (DialLocation >= LockCode[LockLevel - 1] && DialLocation <= LockCode[LockLevel - 1] + PickRange) {
+			if (clockwise) {
+				DialTimeline.Reverse();
+			}
+			else {
+				DialTimeline.Play();
+			}
+			clockwise = !clockwise;
+			LockLevel++;
+		}
+		else {
+			DialTimeline.Stop();
+			ResetDialTimeline.Stop();
+
+			ResetDialTimeline.PlayFromStart();
+		}
+
+		if (LockLevel > LockLength) {
+			// Win Puzzle
+			GetWorldTimerManager().SetTimer(BreakPuzzleTimer, this, &ALockBreak::WinPuzzle, 0.6, false);
+			ExitPuzzle();
+		}
+	}
+}
+
+void ALockBreak::WinPuzzle()
+{
+	// Spawn Destroy Puzzle Actor
+	AActor* breakLockBreak = GetWorld()->SpawnActor<AActor>(BreakLockActor, GetActorLocation(), GetActorRotation());
+
+	// Destroy this
+	Destroy();
+}
+
+// Called every frame
+void ALockBreak::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	DialTimeline.TickTimeline(DeltaTime);
+	ResetDialTimeline.TickTimeline(DeltaTime);
+
+}
+
+void ALockBreak::OnOverlapStart(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	ATBCCharacter* OtherTBCCharacter = Cast<ATBCCharacter>(OtherActor);
+	if (OtherTBCCharacter) {
+		if (OtherTBCCharacter->Tags.Num() > 0)
+		{
+
+			FName PlayerTag = OtherTBCCharacter->Tags[0];
+			TArray<AActor*> FoundActors;
+
+			UGameplayStatics::GetAllActorsWithTag(GetWorld(), PlayerTag, FoundActors);
+
+			if (FoundActors.Num() > 0)
+			{
+				EnableInput(GetWorld()->GetFirstPlayerController());
+				InteractComponent->SetVisibility(true);
+
+				HitBoxPlayer = Cast<ATBCCharacter>(FoundActors[0]);
+				inOverlap = true;
+				//GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Cyan, "Found Permanent Actor Reference");
+			}
+		}
+	}
+}
+
+void ALockBreak::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	inOverlap = false;
+	//GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Cyan, FString::Printf(TEXT("Left Hitbox")));
+	if (HitBoxPlayer) {
+		DisableInput(GetWorld()->GetFirstPlayerController());
+		InteractComponent->SetVisibility(false);
+	}
+}
---
+++ Source/TheBrentCave/Public/Puzzles/LockBreak.h
@@ -0,0 +1,136 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/TimelineComponent.h"
+#include "Components/BoxComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Components/SpotLightComponent.h"
+#include "TimerManager.h"
+#include "TBCCharacter.h"
+#include "GameFramework/Actor.h"
+#include "LockBreak.generated.h"
+
+UCLASS()
+class THEBRENTCAVE_API ALockBreak : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ALockBreak();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere)
+		USceneComponent* Pivot;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+		UStaticMeshComponent* ChestTopMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+		UStaticMeshComponent* ChestBottomMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+		UStaticMeshComponent* LockBaseMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+		UStaticMeshComponent* LockDialMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+		UStaticMeshComponent* LockHookMesh;
+
+	UPROPERTY(EditAnywhere)
+		UCurveFloat* DialCurve;
+
+	UPROPERTY(EditAnywhere)
+		UCurveFloat* ResetDialCurve;
+
+	UPROPERTY(EditAnywhere)
+		USpotLightComponent* Spotlight;
+
+	UPROPERTY(EditAnywhere)
+		UCameraComponent* PuzzleCamera;
+
+	UPROPERTY(EditAnywhere)
+		UBoxComponent* Hitbox;
+
+	UPROPERTY(EditAnywhere)
+		UWidgetComponent* InteractComponent;
+
+	UPROPERTY(EditAnywhere)
+		USoundCue* OnComboSound;
+
+	UPROPERTY(EditAnywhere)
+		TSubclassOf<AActor> BreakLockActor;
+
+	bool ComboRecentlyPlayed;
+
+	FTimeline DialTimeline;
+	FTimeline ResetDialTimeline;
+
+	FTimerHandle ResetCooldown;
+	FTimerHandle BreakPuzzleTimer;
+
+	void SetupTimelines();
+
+	UFUNCTION()
+		void UpdateTimelineProgress(float Value);
+
+	UFUNCTION()
+		void UpdateResetTimelineProgress(float Value);
+
+	void SetupPuzzle();
+	void SetupPlayerInput();
+
+	void Interact();
+
+	void EnterPuzzle();
+	void ExitPuzzle();
+
+	bool inPuzzle;
+	bool inOverlap;
+
+	UFUNCTION()
+		void ResetLock();
+
+	UFUNCTION()
+		void StartPuzzle();
+
+	bool clockwise;
+
+	UPROPERTY(EditAnywhere, Category = "Puzzle Data")
+		float PickRange;
+
+	UPROPERTY(EditAnywhere, Category = "Puzzle Data")
+		int LockLength;
+
+	int LockLevel;
+
+	TArray<int> LockCode;
+	void GenerateLockCode();
+
+	float DialLocation;
+	void LockPickCheck();
+
+	void WinPuzzle();
+
+	bool resettingLock;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY()
+		ATBCCharacter* HitBoxPlayer;
+
+	UFUNCTION()
+		void OnOverlapStart(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+		void OnOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+};
