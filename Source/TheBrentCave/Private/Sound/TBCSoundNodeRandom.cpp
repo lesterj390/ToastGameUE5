a8bfcfd7e652f7569f63b5b63327c67172929ac7
lesterj390 <lesterj390@gmail.com>
2023-08-22 22:56:46 -0400

    Added custom sound node files

---
+++ Source/TheBrentCave/Private/Sound/TBCSoundNodeRandom.cpp
@@ -0,0 +1,5 @@
+// Don't steal out game please :( we worked hard on it!
+
+
+#include "Sound/TBCSoundNodeRandom.h"
+
---
+++ Source/TheBrentCave/Public/Sound/TBCSoundNodeRandom.h
@@ -0,0 +1,17 @@
+// Don't steal out game please :( we worked hard on it!
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Sound/SoundNodeRandom.h"
+#include "TBCSoundNodeRandom.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEBRENTCAVE_API UTBCSoundNodeRandom : public USoundNodeRandom
+{
+	GENERATED_BODY()
+	
+};
